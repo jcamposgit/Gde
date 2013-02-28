49eaa3fe5408b144a92efeee897b5cf86e84f4f9
jose campos <jose.figueiredo.campos@gmail.com>
2013-02-28 15:47:31 +0000

    web matrix delete old

--- trunk/src/Orchard.Web/Modules/RelationSample/Models/RewardProgramRecord.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RelationSample.Models {
-    public class RewardProgramRecord {
-        public virtual int Id { get; set; }
-        public virtual string Name { get; set; }
-        public virtual double Discount { get; set; }
-    }
-}
--- trunk/src/Orchard.Web/Modules/RelationSample/Models/SponsorPartRecord.cs
+++
@@ -1,8 +0,0 @@
-﻿using Orchard.ContentManagement;
-using Orchard.ContentManagement.Records;
-
-namespace RelationSample.Models {
-    public class SponsorPartRecord : ContentPartRecord {
-        public virtual ContentItemRecord Sponsor { get; set; }
-    }
-}
